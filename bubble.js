514fddf8372b09bd4d6b2523b676f0d0b13a4144
sonalsharma1108 <31570014+sonalsharma1108@users.noreply.github.com>
2018-06-14 15:01:21 +0530

    Add files via upload

---
+++ bubble.js
@@ -0,0 +1,14 @@
+function bubbleSort(arr){
+    var len = arr.length;
+    for (var i = len-1; i>=0; i--){
+      for(var j = 1; j<=i; j++){
+        if(arr[j-1]>arr[j]){
+            var temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+         }
+      }
+    }
+    return arr;
+ }
+ bubbleSort([8,6,3,5,4,9]);
